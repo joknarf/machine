1843039bc4175d6213256582c5e09c1b7a5da79b
joknarf <joknarf@free.fr>
2018-12-20 12:20:33 +0100

    new solution

--- machine.py
+++ machine.py
@@ -14,17 +14,18 @@ class Coin(Enum):
     DOLLAR = 100
 
 class Machine:
-    def __init__(self, racks, coint_count = 0):
+    def __init__(self, racks, coin_count=0):
         self.racks = {}
         self.coins = {
-            Coin.NICKEL: coint_count,
-            Coin.DIME: coint_count,
-            Coin.QUARTER: coint_count,
-            Coin.DOLLAR: coint_count
+            Coin.DOLLAR: coin_count,
+            Coin.QUARTER: coin_count,
+            Coin.DIME: coin_count,
+            Coin.NICKEL: coin_count,
         }
         for rack in racks:
             self.racks[rack.code] = rack
         self.amount = 0
+        self.coins_list = list(self.coins.keys()) # need ordered list
 
     def refill(self, code, quantity):
         self.racks[code].quantity += quantity
@@ -33,24 +34,49 @@ class Machine:
         self.coins[coin] += 1
         self.amount += coin.value
 
-    def press(self, code):
-        rack = self.racks[code]
-        if self.amount >= rack.price:
-            if rack.quantity > 0:
-                rack.quantity -= 1
-                change = self.amount - rack.price
-                if change > 0:
-                    self.__give_change(change)
-                self.amount -= rack.price
-            else:
-                self.__give_change(self.amount)
-                print("product not available")
-        else:
-            print("not enough money")
+    # Recursive calls to find right coins combination to give change
+    def __calc_change(self, change, coins, val=0, coins_i=0):
+        if val == change:
+            return coins
+        if val > change:
+            return None
+        for i in range(coins_i, len(coins)):
+            coin_type = self.coins_list[i]
+            if coins[coin_type] == 0:
+                continue
+            ccoins = coins.copy()
+            ccoins[coin_type] -= 1
+            ccoins = self.__calc_change(change, ccoins, val+coin_type.value, i)
+            if ccoins:
+                return ccoins
+        return None
 
     def __give_change(self, change):
-        for coin in reversed(Coin):
-            count = change // coin.value
-            if count <= self.coins[coin]:
-                change = change % coin.value
-                self.coins[coin] -= count
+        new_coins = self.__calc_change(change, self.coins)
+        if new_coins:
+            back_coins = []
+            for key, val in self.coins.items():
+                nbc = val - new_coins[key]
+                if nbc > 0:
+                    back_coins.append(f'{nbc} {key.name}')
+            print(f'Change: {change} ({", ".join(back_coins)})')
+            self.coins = new_coins
+            self.amount = 0
+            return True
+        return False
+
+
+    def press(self, code):
+        if self.amount >= self.racks[code].price:
+            if self.racks[code].quantity > 0:
+                change = self.amount - self.racks[code].price
+                if self.__give_change(change):
+                    self.racks[code].quantity -= 1
+                else:
+                    print("no change available")
+                    self.__give_change(self.amount)
+            else:
+                print("product not available")
+                self.__give_change(self.amount)
+        else:
+            print("not enough money")
